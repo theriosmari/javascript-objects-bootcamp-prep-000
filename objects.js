fc7cb7408796d1b100e2b61362db24da71477c31
Mariel Ríos <marielbrios09@gmail.com>
2017-12-29 06:25:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {Rihanna: "Same Ol' Mistakes", Stereolab: "Lo Boob Oscillator"};
+var playlist = {Rihanna: "Same Ol' Mistakes", Stereolab: "Lo Boob Oscillator", "Kanye": "Gold Digger"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
