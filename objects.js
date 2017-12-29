c72b2ecf397f8949032c497e3ac50bc9d4eb3453
Mariel Ríos <marielbrios09@gmail.com>
2017-12-29 06:23:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName) {
+function rremoveFromPlaylist(playlist, artistName) {
   delete playlist.artistName
   return playlist
 }
